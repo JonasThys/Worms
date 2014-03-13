a65906ec46e51e6dad8f90b89fc2a0482a8a0f77
Jerre1990 <jeroenreinenbergh@MacBook-Pro-van-Jeroen.local>
2014-03-13 10:57:04 +0100

    JumpKlaar
    Ook vergelijken van doubles heb ik in heel de klasse al aangepast zoals
    in de opgave staat.

--- src/worms/model/Worm.java
+++ src/worms/model/Worm.java
@@ -1,6 +1,7 @@
 package worms.model;
 
 import be.kuleuven.cs.som.annotate.*;
+import worms.util.*;
 
 /**
  * 
@@ -18,15 +19,15 @@ public class Worm {
 	 * Initialize this new worm with given name, given x-coordinate, given y-coordinate,
 	 * given direction and given radius.
 	 * 
-	 * @param	name (def)
+	 * @param	name
 	 * 			The name of the worm.
-	 * @param 	x (def)
+	 * @param 	x
 	 * 			The x-coordinate of the worm's position expressed in metres.
-	 * @param	y (def)
+	 * @param	y
 	 * 			The y-coordinate of the worm's position expressed in metres.
-	 * @param	direction (nominal)
+	 * @param	direction
 	 * 			The direction towards which the worm faces expressed in radians.
-	 * @param	radius (def)
+	 * @param	radius
 	 * 			The radius of the spherical body of the worm expressed in metres.
 	 * @post 	The new name of this worm is equal to the given name.
 	 * 		|	new.getName() = name
@@ -217,7 +218,7 @@ public class Worm {
 	 * 		|	result == (radius >= lowerBoundOfRadius)
 	 */
 	private boolean isPossibleRadius(double radius){
-		return radius >= lowerBoundOfRadius;
+		return Util.fuzzyGreaterThanOrEqualTo(radius, lowerBoundOfRadius);
 	}
 	
 	/**
@@ -320,7 +321,7 @@ public class Worm {
 	 * @param 	numberOfSteps
 	 * 			The number of steps to be taken by the worm in the current direction.
 	 * @return	The amount of action points to be paid equals the number of steps times the weighted direction.
-	 * 		|	result == numberOfSteps*(ceil(abs(cos(direction))+abs(4*sin(direction))))
+	 * 		|	result == numberOfSteps*(ceil(|cos(direction)|+|4*sin(direction)|))
 	 * @throws 	IllegalArgumentException("Invalid number of steps!")
 	 * 			The given number of steps is not a possible number of steps for any worm.
 	 * 		|	! isPossibleNumberOfSteps(numberOfSteps)
@@ -341,6 +342,9 @@ public class Worm {
 	 * 			The number of steps to be taken by the worm in the current direction.
 	 * @return	True if and only if the current number of action points is not smaller than the amount of action points required to move the worm with the given number of steps.
 	 * 		|	result == (numberOfActionPoints >= amountOfActionPointsForMoving(numberOfSteps))
+	 * @throws 	IllegalArgumentException("Invalid number of steps!")
+	 * 			The given number of steps is not a possible number of steps for any worm.
+	 * 		|	! isPossibleNumberOfSteps(numberOfSteps)
 	 */	
 	public boolean canMove(int numberOfSteps) throws IllegalArgumentException {
 		if (! isPossibleNumberOfSteps(numberOfSteps))
@@ -368,8 +372,8 @@ public class Worm {
 		double incrementY = Math.sin(direction) * radius;
 		double newX = x + (numberOfSteps * incrementX);
 		double newY = y + (numberOfSteps * incrementY);
-		this.setX(newX);
-		this.setY(newY);
+		setX(newX);
+		setY(newY);
 	}
 	
 	/** 
@@ -383,16 +387,11 @@ public class Worm {
 	 * 		|	new.getY() == this.getY() + Math.sin(direction)*numberOfSteps*radius
 	 * @post	The new number of action points equals the old number of action points reduced with the amount of action points to be paid for moving the given number of steps.
 	 * 		|	new.getNumberOfActionPoints() == this.getNumberOfActionPoints - amountOfActionPointsForMoving(numberOfSteps)
-	 * @throws 	IllegalArgumentException("Invalid number of steps!")
-	 * 			The given number of steps is not a possible number of steps for any worm.
-	 * 		|	! isPossibleNumberOfSteps(numberOfSteps)
 	 * @throws 	UnsupportedOperationException("Cannot move!")
 	 * 			The worm cannot move the given number of steps.
 	 * 		|	! canMove(numberOfSteps)
 	 */
 	public void activeMove(int numberOfSteps) throws IllegalArgumentException {
-		if (! isPossibleNumberOfSteps(numberOfSteps))
-			throw new IllegalArgumentException("Invalid number of steps!");
 		if (! canMove(numberOfSteps))
 			throw new UnsupportedOperationException("Cannot move!");
 		move(numberOfSteps);
@@ -459,4 +458,108 @@ public class Worm {
 		turn(turnByAngle);
 		setNumberOfActionPoints(numberOfActionPoints - amountOfActionPointsForTurning(turnByAngle));
 	}
+	
+	/**
+	 * Return the initial velocity of this worm during a jump.
+	 * 
+	 * @return	The initial velocity of the worm equals the quotient of a certain force and the worm's mass, divided by two.
+	 * 			This force can be calculated as the sum of five times the worm's remaining number of action points on the one hand and its mass times Earth's standard acceleration coefficient on the other hand.
+	 * 		|	result == (force / getMass()) * 0.5
+	 * 		|	force == (5 * numberOfActionPoints) + (getMass() * standardAcceleration)
+	 */
+	private double initialVelocity(){
+		double force = (5 * numberOfActionPoints) + (getMass() * standardAcceleration);
+		return ((force / getMass()) * 0.5);
+	}
+	
+	/**
+	 * Return the horizontal distance covered by this worm during a jump.
+	 * 
+	 * @return	The horizontal jumping distance of this worm is equal to the product of its squared initial velocity, the sinus of its doubled direction and the inverse of Earth's standard acceleration coefficient.
+	 * 		|	result == (initialVelocity()^2 * sin(direction * 2)) / standardAcceleration
+	 */	
+	private double horizontalJumpDistance(){
+		return ((Math.pow(initialVelocity(), 2) * Math.sin(direction * 2)) / standardAcceleration);
+	}
+	
+	/**
+	 * Return the time passed after a jump of this worm.
+	 * 
+	 * @return	The jump time equals the absolute value of the quotient of the worm's horizontal jump distance and the product of its initial velocity and the cosinus of its direction.
+	 * 		|	result == |horizontalJumpDistance() / (initialVelocity() * Math.cos(direction))|
+	 */	
+	public double jumpTime(){
+		return (Math.abs(horizontalJumpDistance() / (initialVelocity() * Math.cos(direction))));
+	}
+	
+	/**
+	 * Return the x-coordinate of this worm during a jump after the given amount of time that has already passed.
+	 * 
+	 * @return	The in-jump x-coordinate of this worm after the given amount of time that has passed is equal to the worm's initial x-coordinate
+	 * 			incremented with the product of its initial velocity, the cosinus of its direction and the given time that has passed.
+	 * 		|	result == x + (initialVelocity() * Math.cos(direction) * timePassed)
+	 */	
+	private double jumpStepOnXAxis(double timePassed){
+		return (x + (initialVelocity() * Math.cos(direction) * timePassed));
+	}
+	
+	/**
+	 * Return the y-coordinate of this worm during a jump after the given amount of time that has already passed.
+	 * 
+	 * @return	The in-jump y-coordinate of this worm after the given amount of time that has passed is equal to the worm's initial y-coordinate
+	 * 			incremented with the product of its initial velocity, the sinus of its direction and the given time that has passed,
+	 * 			and decremented with the product of Earth's standard acceleration coefficient, the squared time that has passed and the constant 0.5.
+	 * 		|	result == y + ((initialVelocity() * Math.sin(direction) * timePassed) - ((1/2) * standardAcceleration * timePassed^2))
+	 */		
+	private double jumpStepOnYAxis(double timePassed){
+		return (y + ((initialVelocity() * Math.sin(direction) * timePassed) - ((1/2) * standardAcceleration * Math.pow(timePassed, 2))));
+	}	
+	
+	/**
+	 * Return the coordinates of this worm during a jump after the given amount of time that has already passed.
+	 * 
+	 * @return	The array of in-jump coordinates of this worm after the given amount of time that has passed consists of the appropriate x-coordinate and y-coordinate respectively.
+	 * 			Both coordinates equal the distance traveled on the appropriate axis during the jump after the given time that has already passed.
+	 * 		|	result[0] == jumpStepOnXAxis(timePassed)
+	 * 		|	result[1] == jumpStepOnYAxis(timePassed)
+	 */	
+	public double[] jumpStep(double timePassed){
+		double [] coordinatesAfterJumpStep = {jumpStepOnXAxis(timePassed),jumpStepOnYAxis(timePassed)};
+		return coordinatesAfterJumpStep;
+	}
+	
+	/**
+	 * Check whether the worm can jump.
+	 * 
+	 * @return	True if and only if the direction of this worm is not greater than pi.
+	 * 		|	result == (direction <= Math.PI)
+	 */	
+	private boolean canJump(){
+		return Util.fuzzyLessThanOrEqualTo(direction, Math.PI);
+	}
+	
+	/**
+	 * Make this worm jump in the current direction.
+	 * 
+	 * @post	The new X-coordinate of the worm is equal the distance traveled on the x-axis during the time of the jump.
+	 * 		|	new.getX() == jumpStepOnXAxis(jumpTime())
+	 * @post	The new Y-coordinate of the worm is equal to the old Y-coordinate.
+	 * 		|	new.getY() == this.getY()
+	 * @post	The new number of action points of the worm is equal to zero.
+	 * 		|	new.getNumberOfActionPoints() == 0
+	 * @throws 	UnsupportedOperationException("Cannot jump!")
+	 * 			The worm cannot jump.
+	 * 		|	! canJump()
+	 */	
+	public void jump(){
+		if(! canJump())
+			throw new UnsupportedOperationException("Cannot jump!");
+		setX(jumpStepOnXAxis(jumpTime()));
+		setNumberOfActionPoints(0);
+	}
+	
+	/**
+	 * Constant representing the approximated value of Earth's standard acceleration coefficient.
+	 */	
+	private final double standardAcceleration = 9.80665;
 }
